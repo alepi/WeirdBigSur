c7b298281a6e28c2885400dada6e29bd8fc08092
alepi <667578+alepi@users.noreply.github.com>
2020-12-26 14:38:48 +0100

    this solution seems to work fine

--- Weird/CustomView.swift
+++ Weird/CustomView.swift
@@ -11,9 +11,32 @@ class CustomView: NSView {
     
     var image = NSImage(named: "test")!
     
-    override func draw(_ dirtyRect: NSRect) {
-        super.draw(dirtyRect)
-        image.draw(in: bounds)
+    fileprivate func drawImage(in ctx: CGContext) {
+        ctx.setShouldAntialias(false)
+        ctx.interpolationQuality = .none
+        var r = bounds
+        let cgImage = image.cgImage(forProposedRect: &r, context: nil, hints: nil)!
+        ctx.draw(cgImage, in: bounds)
     }
     
+    override func makeBackingLayer() -> CALayer {
+        let layer = CALayer()
+        layer.delegate = self
+        layer.needsDisplayOnBoundsChange = true
+        layer.frame = bounds
+        layer.minificationFilter = .nearest
+        layer.magnificationFilter = .nearest
+        return layer
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        wantsLayer = true
+    }
+}
+
+extension CustomView: CALayerDelegate {
+    func draw(_ layer: CALayer, in ctx: CGContext) {
+        drawImage(in: ctx)
+    }
 }
